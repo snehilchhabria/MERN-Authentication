7c5a44e94b7f97778d6f476b6a8ce0cdeb11fda8
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-08-31 22:34:19 +0530

    handler and routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,11 @@
+import asyncHandler from 'express'
+
 // @desc  Auth user/set token
 // routes  POST /api/users/auth
 // @access Public
- const authUser = (req,res) => {
+ const authUser = asyncHandler( async(req,res) => {
     res.status(200).json({message: 'Auth User'})
- }
+ })
 
  export {
     authUser
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const router  = express.Router()
-import { authUser } from '../controllers/userController'
+import { authUser } from '../controllers/userController.js'
 
 
 router.post('/auth', authUser)
