8fa9f32d9ea24062ce11cdd0d1c5fccb0771ffaa
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-09-02 00:29:32 +0530

    register user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,7 +11,22 @@ import User from '../models/userModel.js';
 // @desc  Register a new user
 // routes  POST /api/users/
 // @access Public
- const registerUser = asyncHandler(async(req,res) => {
+ const registerUser = asyncHandler(async(req,res) => {    
+
+   const { name, email, password } = req.body;
+
+   const userExists = await User.findOne({ email });
+
+   if(userExists) {
+      res.status(400);
+      throw new Error('User already exists');
+   }
+   const user = await User.create({
+      name, 
+      email,
+      password
+   });
+    
     res.status(200).json({message: 'Register User'})
  });
 
