c6e3b6ab585edce1d9877859b8617a3ac76da2e5
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-09-03 16:57:27 +0530

    update user profile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -84,8 +84,28 @@ import User from '../models/userModel.js';
 // @desc  Update user profile
 // routes  PUT /api/users/profile
 // @access Private
- const updateUserProfile = asyncHandler(async(req,res) => {
-    res.status(200).json({message: 'Update    User Profile'})
+ const updateUserProfile = asyncHandler(async (req,res) => {
+    
+   const user = await User.findById(req.user._id);
+
+   if(user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+
+      if(req.body.password) {
+         user.password = req.body.password;
+      }
+      const updatedUser = await user.save();
+      res.status(200).json({
+         _id: updatedUser._id,
+         name: updatedUser.name,
+         emial: updatedUser.email
+      })
+
+   } else {
+      res.status(404);
+      throw new Error('User not found');
+   }
  });
 
  export {
