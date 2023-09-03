3a860a3e228103ffd4007972828746ad9dff61f2
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-09-03 16:41:07 +0530

    getUser route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,8 +70,15 @@ import User from '../models/userModel.js';
 // @desc  Get user profile
 // routes  GET /api/users/profile
 // @access Private
- const getUserProfile = asyncHandler(async(req,res) => {
-    res.status(200).json({message: 'User Profile'})
+ const getUserProfile = asyncHandler(async (req,res) => {
+
+   const user = {
+      _id: req.user._id,
+      name: req.user.name,
+      email: req.user.email
+   }
+
+    res.status(200).json(user);
  });
 
 // @desc  Update user profile
