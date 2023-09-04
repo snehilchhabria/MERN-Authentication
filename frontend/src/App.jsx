60e32e4c7f1dac748cd48a7d26abb675354a0228
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-09-04 15:36:51 +0530

    homescreen setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import Header from './components/header';
-
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
     <Header />
-    <h1>MERN App</h1>
+    <HomeScreen />
     </>
   )
 }
