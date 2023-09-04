2b1e9abcde1adc6e4719957ebc919e3d80434891
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-09-04 16:25:11 +0530

    LinkContainer

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,17 +7,23 @@ const Header = () => {
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href='/'>MERN Auth</Navbar.Brand>
+                    <LinkContainer to='/'>
+                    <Navbar.Brand>MERN Auth</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
                         <Navbar.Collapse id='basic-navbar-nav'>
-                        <Nav className='ms-auto' >
-                            <Nav.Link href='/login'>
-                                <FaSignInAlt /> Sign In
-                            </Nav.Link>
-                            <Nav.Link href='/register'>
-                                <FaSignInAlt /> Sign Up
-                            </Nav.Link>
-                        </Nav>
+                            <Nav className='ms-auto'>
+                                <LinkContainer to='/login'>
+                                <Nav.Link>
+                                    <FaSignInAlt /> Sign In
+                                </Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to='/register'>
+                                <Nav.Link>
+                                    <FaSignOutAlt /> Sign Up
+                                </Nav.Link>
+                                </LinkContainer>
+                            </Nav>
                         </Navbar.Collapse>
                 </Container>
             </Navbar>
