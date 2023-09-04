f624d88f514c45004faaf8402a333ed8263b4456
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-09-04 16:02:06 +0530

    updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,11 +9,12 @@ import {
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+import HomeScreen from './screens/HomeScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements (
     <Route path='/' element={<App />}>
-
+      <Route index={true} path='/' element={<HomeScreen />} />
     </Route>
 )
 )
