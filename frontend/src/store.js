a67d168f671eb819867190a6223e963f7c97a2a5
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-09-05 22:48:30 +0530

    authSlice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+
  const store = configureStore({
-    reducer: {},
+    reducer: {
+        auth: authReducer,
+    },
     middlware: (getDefaultMiddleware) => getDefaultMiddleware(),
     devTools: true
  })
