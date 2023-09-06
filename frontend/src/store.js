8e2ee686608534c3b08217ddba84f0dce156a802
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-09-07 01:04:05 +0530

    apiSlice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import { apiSlice } from './slices/apiSlice';
 
  const store = configureStore({
     reducer: {
         auth: authReducer,
+        [apiSlice.reducerPath] : apiSlice.reducer,
     },
-    middlware: (getDefaultMiddleware) => getDefaultMiddleware(),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
     devTools: true
  })
 
