12f318af3fd31fdf16a78832a8e3f880a7830147
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-09-07 22:11:44 +0530

    debug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/header';
 import { ToastContainer } from 'react-toastify';
-import 'react/toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
